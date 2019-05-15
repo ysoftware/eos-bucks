e00d851199c70afdaf8b9fc3bd4f0c97d4c12791
Yaroslav Erohin <ysoftware@users.noreply.github.com>
2019-05-15 19:39:09 +0300

    fix oracle

--- src/oracle.hpp
+++ src/oracle.hpp
@@ -36,27 +36,24 @@ void buck::_update(uint32_t eos_price, bool force) {
   const uint32_t previous_price = stats.oracle_eos_price;
   int32_t new_price = eos_price;
   
-  if (new_price != previous_price) {
+  // shave off price change if don't have a special permission
+  if (!force && previous_price != 0) {
+    const uint32_t difference = std::abs(int32_t(previous_price) - int32_t(new_price));
+    const uint32_t percent = difference * 100 / previous_price;
     
-    // shave off price change if don't have a special permission
-    if (!force && previous_price != 0) {
-      const uint32_t difference = std::abs(int32_t(previous_price) - int32_t(new_price));
-      const uint32_t percent = difference * 100 / previous_price;
-      
-      // handle situations when any price difference is too big
-      if (percent > ORACLE_MAX_PERCENT) {
-        new_price = previous_price + (previous_price > new_price ? -1 : 1);
-      }
+    // handle situations when any price difference is too big
+    if (percent > ORACLE_MAX_PERCENT) {
+      new_price = previous_price + (previous_price > new_price ? -1 : 1);
     }
-    
-    // protect from 0 price
-    new_price = std::max(1, new_price);
-    
-    _stat.modify(stats, same_payer, [&](auto& r) {
-      r.oracle_timestamp = get_current_time_point();
-      r.oracle_eos_price = new_price;
-    });
   }
+  
+  // protect from 0 price
+  new_price = std::max(1, new_price);
+  
+  _stat.modify(stats, same_payer, [&](auto& r) {
+    r.oracle_timestamp = get_current_time_point();
+    r.oracle_eos_price = new_price;
+  });
   
   set_processing_status(ProcessingStatus::processing_cdp_requests);
   
