13cb094d770dc12f472c96322a58341898abe6ab
Yaroslav Erohin <ysoftware@users.noreply.github.com>
2019-05-22 23:16:07 +0300

    disable testing

--- src/constants.hpp
+++ src/constants.hpp
@@ -3,11 +3,11 @@
 // Created by Yaroslav Erohin and Dmitry Morozov.
 
 /// defines if contract will print logs and enable debug features
-#define DEBUG true
+#define DEBUG false
 /// defines if contract uses test rex environment
-#define REX_TESTING true
+#define REX_TESTING false
 /// defines if testing of time points is enabled
-#define TEST_TIME true
+#define TEST_TIME false
 
 static const uint32_t seconds_per_day     = 86'400;
 static const uint32_t YEAR                = 31'557'600;
