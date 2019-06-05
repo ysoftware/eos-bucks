8c9f59ba063514e58607f329a849f4f94021285c
Yaroslav Erohin <ysoftware@users.noreply.github.com>
2019-06-05 09:50:41 +0300

    one more sanity check

--- src/tax.hpp
+++ src/tax.hpp
@@ -137,6 +137,7 @@ void buck::remove_excess_collateral(const cdp_i::const_iterator& cdp_itr) {
   
   _tax.modify(tax, same_payer, [&](auto& r) {
     check(r.total_excess >= excess, "programmer error, total excess underflow");
+    check(r.aggregated_excess >= agec, "programmer error, aggregated excess underflow");
     
     r.total_excess -= excess;
     r.aggregated_excess -= agec;
